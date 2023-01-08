5036424a2581f8817a0c1abd838056097c06b1f6
AnukratiDW <120546412+AnukratiDW@users.noreply.github.com>
2023-01-08 03:10:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
     else: 
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
- 
+
 streamlit.header('The fruit load contains:') 
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -58,3 +58,14 @@ if streamlit.button ('Get Fruit Load List'):
 
 except URLError as e:
     streamlit.error()
+    
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " +new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
