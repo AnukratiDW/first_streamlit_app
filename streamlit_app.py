e7897e2100d2ef86a55cdc4ba8f59c6db140461f
AnukratiDW <120546412+AnukratiDW@users.noreply.github.com>
2023-01-08 00:56:35 -0500

    testing overloading

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,9 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-fruit_choice_1 = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice_1)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ',add_my_fruit)
+
+#This will not work correctly, but just go with it for now 
+my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
 
