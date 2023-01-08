51ec60c78c55415d8580a1c68a516abde7b27bbb
AnukratiDW <120546412+AnukratiDW@users.noreply.github.com>
2023-01-08 01:39:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 import pandas
-import requests
 import snowflake.connector
 import urllib.error 
 
@@ -15,7 +14,6 @@ streamlit.text('🐔 Hard Boiled Free Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
